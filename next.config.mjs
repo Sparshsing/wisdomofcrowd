4b11a1a23eacf553a618740eeb2e1c103f42db0b
sparshsing <sparshsing96@gmail.com>
2025-01-10 23:25:51 +0530

    Add GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/wisdomofcrowd",
+  assetPrefix: "/wisdomofcrowd",
+};
 
 export default nextConfig;
